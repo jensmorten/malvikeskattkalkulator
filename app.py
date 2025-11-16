fb65ba613c964249e5da718ef112e28dd3d4ae5a
jensmorten <jens.morten.nilsen@gmail.com>
2025-11-16 12:31:49 +0100

    Update app.py

--- app.py
+++ app.py
@@ -198,7 +198,7 @@ total_skatt_ny = df["Skatt_ny"].sum()
 total_skatt_ny=total_skatt_ny*1.02
 
 st.subheader("🔮 Ny berekna eigedomsskatt (2026)")
-total_mill = round(total_skatt_ny / 1_000_000,2)
+total_mill = round(total_skatt_ny / 1_000_000,1)
 st.metric(
     label="",
     value=f"{total_mill} mill. kr"
@@ -208,7 +208,7 @@ st.subheader("💁‍♂️ Kommunedirektørens forslag (2026)")
 kd_total_mill = 35.8
 st.metric(
     label="",
-    value=f"{total_mill} mill. kr"
+    value=f"{kd_total_mill} mill. kr"
 )
 
 
