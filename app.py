a200744ebc1cc5089ad34124da6dcee27f31dd32
jensmorten <jens.morten.nilsen@gmail.com>
2026-06-19 10:51:26 +0200

    Update app.py

--- app.py
+++ app.py
@@ -198,7 +198,7 @@ df["Skatt_ny"] = (
 
 total_skatt_ny = df["Skatt_ny"].sum()
 ##jenks konstant
-total_skatt_ny=total_skatt_ny*1.045 ###
+total_skatt_ny=total_skatt_ny*1.04 ###
 #total_skatt_ny=total_skatt_ny*0.997
 
 st.subheader("🔮 Kalkulaorens berekna eigedomsskatt (2026)")
